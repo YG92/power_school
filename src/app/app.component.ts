80c1cc550a208872686057d32aad77784ee2cb1a
julia galiulina <julie_gal@mail.ru>
2017-09-02 14:07:08 +0500

    static elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Stats, FooterList } from './objects';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './fontello/css/fontello.css']
 })
 export class AppComponent {
-  
+  stat_list = [
+    new Stats('32m', 'students'),
+    new Stats('66m', 'parents'),
+    new Stats('100m', 'users'),
+    new Stats('13,000', 'districts'),
+  ];
+
+  footer_list = [
+    new FooterList('Solutions', true),
+    new FooterList('K-12 Impact', true),
+    new FooterList('Community', false),
+    new FooterList('Resources', true),
+    new FooterList('Company', true),
+    new FooterList('Get Started', false),
+  ]
+
+  socials = [
+    'icon-twitter',
+    'icon-facebook',
+    'icon-linkedin',
+    'icon-youtube-play',
+    'icon-gplus'
+  ]
 }
---
+++ src/app/objects.ts
@@ -0,0 +1,13 @@
+export class Stats {
+  constructor(
+    public num: string,
+    public title: string
+  ) {}
+}
+
+export class FooterList {
+  constructor(
+    public title: string,
+    public arrow: boolean
+  ) {}
+}
